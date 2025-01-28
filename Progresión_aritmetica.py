7a2a26a172a5ca238139600ba71c3f1dc52c109a
MatiaS-Coding <matias.salas.ok@gmail.com>
2025-01-28 16:06:31 -0300

    Update Progresión_aritmetica.py

--- Progresión_aritmetica.py
+++ Progresión_aritmetica.py
@@ -1,5 +1,3 @@
-# Maria Carmen ingresa $18.000 al en un banco que le ofrece un tipo de interés anual del 3% durante 5 años. Hay dos calculos posibles, el del interés simple que (progreción aritmética) 
-# y el interés compuesto con al añadir a ese monto anual los intereses y reinvertir (progreción geométrica).
 
 Dinero_invertido = 18000
 Interes_anual = 0.03
